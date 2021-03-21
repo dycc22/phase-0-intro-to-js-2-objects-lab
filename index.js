5a2aef778d03193fb29e63ffab67bd84342a2946
David <dycchang@gmail.com>
2021-03-21 18:02:59 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name:"David Smith",
+    streetAddress:"123 Main St"
+};
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+const newemployee = { ...employee }; //clone employee to newemployee
+newemployee[key] = value;
+return newemployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+employee[key] = value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key) {
+const newemployee = { ...employee };
+delete newemployee[key];
+return newemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+delete employee[key];
+return employee;
+}
